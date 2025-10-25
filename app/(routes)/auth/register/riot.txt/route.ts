a3014984fb7612dc5bec2a3d1e1b67714a070d6b
rodrigopozodev <rodrigopozosanchez@gmail.com>
2025-10-25 15:05:22 +0200

    arreglando errores

--- app/(routes)/auth/register/riot.txt/route.ts
+++ app/(routes)/auth/register/riot.txt/route.ts
@@ -1,7 +1,7 @@
 export const dynamic = "force-static";
 
 export async function GET() {
-  const body = "851b03ff-3318-4840-9f0b-388969b8ecb3";
+  const body = "74a5b14d-e20b-4ff9-85c1-17fa41397e06";
   return new Response(body, {
     status: 200,
     headers: {
